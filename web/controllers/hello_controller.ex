20c571ae8055f22cbce4f68e3cc0a24385d6e21b
Neil Gardose <nkpgardose@gmail.com>
2016-11-03 00:32:56 +0800

    Add hello controller with index/2 show/2

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,12 @@
+require IEx;
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+  
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, :show, messenger: messenger
+  end
+end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, :index
   end
 end
